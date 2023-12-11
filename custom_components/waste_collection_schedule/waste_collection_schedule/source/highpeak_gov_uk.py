d5daa0a0aeb8ca0486fe75910e0d02806897c3ba
Steve <steve.loveday@gmail.com>
2023-12-11 19:46:17 +0000

    Update highpeak_gov_uk.py

--- custom_components/waste_collection_schedule/waste_collection_schedule/source/highpeak_gov_uk.py
+++ custom_components/waste_collection_schedule/waste_collection_schedule/source/highpeak_gov_uk.py
@@ -12,7 +12,7 @@ URL = "https://www.highpeak.gov.uk/"
 TEST_CASES = {
     # High Peak Council wants specific addresses, hopefully these are generic enough.
     "Hadfield Conservative Club, 221 Woolley Bridge Road, Hadfield, SK13 1BW": {
-        "premises_id": "71b6cce0-06e1-43e5-999c-56e4f84e3142&fsn=2306292b-e07d-4ba9-b899-ea50d0c98a70"
+        "PAGESESSIONID": "71b6cce0-06e1-43e5-999c-56e4f84e3142&fsn=2306292b-e07d-4ba9-b899-ea50d0c98a70"
     },
     "Pear Tree Inn, Hadfield, SK13 1PY": {"post_code": "SK13 1PY", "house_number": 431},
     "Pizza King, Hadfield, SK13 2DT": {"post_code": "SK13 2DT", "house_number": 44}
@@ -30,28 +30,28 @@ class Source:
     def __init__(
         self, premises_id: int = None, post_code: str = None, house_number: str = None
     ):
-        self._premises_id = premises_id
+        self._PAGESESSIONID = PAGESESSIONID
         self._post_code = post_code
         self._house_number = house_number
-        if not any([self._premises_id, self._post_code and self._house_number]):
+        if not any([self._PAGESESSIONID, self._post_code and self._house_number]):
             raise Exception(
-                "premises_id or post_code and house number must be provided in config"
+                "PAGESESSIONID or post_code and house number must be provided in config"
             )
         self._session = requests.Session()
 
     def fetch(self):
         entries = []
 
-        if self._premises_id is not None:
+        if self._PAGESESSIONID is not None:
             r = requests.get(
                 "https://www.highpeak.gov.uk/findyourbinday",
                 params={
-                    "PremisesId": self._premises_id,
+                    "PAGESESSIONID": self._PAGESESSIONID,
                 },
             )
         else:
             # Property search endpoint redirects you to the page, so by caching
-            # The premises_id in future, we save an extra request every check.
+            # The PAGESESSIONID in future, we save an extra request every check.
             r = requests.get(
                 "https://www.highpeak.gov.uk/findyourbinday",
                 params={
@@ -61,7 +61,7 @@ class Source:
             )
             query = urlsplit(r.url).query
             params = parse_qs(query)
-            self._premises_id = params["PremisesId"].pop()
+            self._PAGESESSIONID = params["PAGESESSIONID"].pop()
 
         soup = BeautifulSoup(r.text, features="html.parser")
         results = soup.find_all("div", {"class": "binresult"})
